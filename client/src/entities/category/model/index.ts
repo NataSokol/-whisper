75e3b0ad0fa09fb04a2d099bc0a4639a06d19497
Excomunicad0 <e_zhul@mail.ru>
2024-10-03 17:05:20 +0300

    created redux, pages, populated the entities

---
+++ client/src/entities/category/model/index.ts
@@ -0,0 +1,18 @@
+export type Category = {
+    id: number;
+    title: string;
+};
+
+export type CategoryList = Category[];
+
+// RESPONSE
+
+export type CategoryResponse = {
+    categories: CategoryList;
+    message: string;
+}
+
+export type OneCategoryResponse = {
+    category: Category;
+    message: string;
+}
