c435ef3ffff9bd9b67ec3269d6a698d95b531cbe
Polina-VA <polinavakhaeva3@gmail.com>
2024-10-04 11:01:20 +0300

    feat: created useCollectionHook and Thunck

--- client/src/entities/collection/model/collectionThunk.ts
+++ client/src/entities/collection/model/collectionThunk.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { CollectionListResponse } from ".";
+import { Collection, CollectionListResponse, CollectionResponse } from ".";
 import { CollectionService } from "../api";
 import { AxiosError } from "axios";
 
@@ -15,7 +15,7 @@ CollectionListResponse,
   { rejectValue: RejectValue }
 >("/getAllCollection", async (_, { rejectWithValue }) => {
   try {
-    return await CollectionService.geyAllCollections();
+    return await CollectionService.getAllCollections();
   } catch (error) {
     const err = error as AxiosError<{ message: string }>;
     return rejectWithValue({
@@ -24,4 +24,53 @@ CollectionListResponse,
   }
 });
 
+export const createCollection = createAsyncThunk<
+CollectionResponse,
+  {  title: string,
+    image: string,
+  },
+  { rejectValue: RejectValue }
+>("/createCollection", async ({title, image}, { rejectWithValue }) => {
+  try {
+   return await CollectionService.createCollection({title, image});
+  } catch (error) {
+    const err = error as AxiosError<{ message: string }>;
+    return rejectWithValue({
+      message: err.response?.data.message || err.message,
+    });
+  }
+});
   
+
+export const updateCollection = createAsyncThunk<
+CollectionResponse,
+  {  id: number,
+    title: string,
+    image: string,
+  },
+  { rejectValue: RejectValue }
+>("/updateCollection", async ({id, title, image}, { rejectWithValue }) => {
+  try {
+   return await CollectionService.updateCollection(id, {title, image});
+  } catch (error) {
+    const err = error as AxiosError<{ message: string }>;
+    return rejectWithValue({
+      message: err.response?.data.message || err.message,
+    });
+  }
+});
+
+export const deleteCollection = createAsyncThunk<
+void,
+  { id: number },
+  { rejectValue: RejectValue }
+>("/deleteCollection", async ({ id }, { rejectWithValue }) => {
+  try {
+    await CollectionService.deleteCollection(id);
+  } catch (error) {
+    const err = error as AxiosError<{ message: string }>;
+    return rejectWithValue({
+      message: err.response?.data.message || err.message,
+    });
+  }
+});
---
+++ client/src/shared/hooks/useCollectionAction.ts
@@ -0,0 +1,50 @@
+import { getAllCollections } from "@/entities/collection";
+import { useAppDispatch } from "./useReduxHooks";
+import { useCallback } from "react";
+import { CollectionCreate } from "@/entities/collection/model";
+import { createCollection, deleteCollection, updateCollection } from "@/entities/collection/model/collectionThunk";
+import { unwrapResult } from "@reduxjs/toolkit";
+
+export const useCollectionAction = () => {
+    const dispatch = useAppDispatch();
+
+    const getCollectionList = useCallback(() => {
+        dispatch(getAllCollections())
+    }, [dispatch])
+
+    const handleCreateCollection = useCallback(() => {
+        async ({title, image} : CollectionCreate) => {
+            try {
+                const result = await dispatch(createCollection({title, image}))
+                unwrapResult(result)
+                dispatch(getAllCollections())
+            } catch (error) {
+                console.log("Error creating collection", error)
+            }
+        }
+    }, [dispatch])
+
+    const handleUpdateCollection = useCallback(() => {
+        async (id : number, title: string, image: string) => {
+                const result = await dispatch(updateCollection({id, title, image}))
+                unwrapResult(result)
+                dispatch(getAllCollections())
+        }
+    }, [dispatch])
+
+    const handleDeleteCollection = useCallback(() => {
+     async (id: number) => {
+         await dispatch(deleteCollection({id}))
+         dispatch(getAllCollections())
+     }   
+    }, [dispatch])
+        
+
+
+    return {
+        getCollectionList,
+        handleCreateCollection,
+        handleUpdateCollection,
+        handleDeleteCollection,
+    };
+};
