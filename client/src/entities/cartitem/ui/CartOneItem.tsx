7f8c8a94106b0e6b60f76c9fb8470d9d6c0aa059
Polina-VA <polinavakhaeva3@gmail.com>
2024-10-10 23:53:15 +0300

    finished cartLogic almost finished cartStyles

--- client/src/entities/cartitem/ui/CartOneItem.tsx
+++ client/src/entities/cartitem/ui/CartOneItem.tsx
@@ -11,7 +11,12 @@ type Props = {
   onHandleDecreaseQuantity: (cartItem: CartItem) => void;
 };
 
-export const CartOneItem: React.FC<Props> = ({ cartItem, handleDelete, onHandleIncreaseQuantity, onHandleDecreaseQuantity }) => {
+export const CartOneItem: React.FC<Props> = ({
+  cartItem,
+  handleDelete,
+  onHandleIncreaseQuantity,
+  onHandleDecreaseQuantity,
+}) => {
   return (
     <div className={styles.cartItemContainer}>
       <Link to={ROUTES.CATALOG + "/" + cartItem.productId}>
@@ -21,28 +26,46 @@ export const CartOneItem: React.FC<Props> = ({ cartItem, handleDelete, onHandleI
         />
       </Link>
       <div className={styles.productInfo}>
-      <button className={styles.delCartItem}>
-        <img
-          src="../../public/img/plus.svg"
-          alt="закрыть"
-          onClick={() => handleDelete(cartItem)}
-        />
-      </button>
-      <Link to={ROUTES.CATALOG + "/" + cartItem.productId}>
-        <h1 className={styles.cartItemTitle}>{cartItem.Product.title}</h1>
-      </Link>
-      <p className={styles.cartItemDetails}>
-        {cartItem.colorProduct.Color.title}
-      </p>
-      <p className={styles.cartItemPrice}>{cartItem.Product.price}</p>
-      <p className={styles.cartItemPrice}>{cartItem.Product.salePrice}</p>
-      <p className={styles.cartItemDetails}>{cartItem.ProductSize.sizeTitle}</p>
-      <button onClick={() => onHandleDecreaseQuantity(cartItem)}>-</button>
-      <p className={styles.cartItemDetails}>Количество:{cartItem.quantity}</p>
-      <button
-        onClick={() => onHandleIncreaseQuantity(cartItem)}>
-        +
-      </button>
+        <button className={styles.delCartItem}>
+          <img
+            src="../../public/img/plus.svg"
+            alt="закрыть"
+            onClick={() => handleDelete(cartItem)}
+          />
+        </button>
+        <Link to={ROUTES.CATALOG + "/" + cartItem.productId}>
+          <h1 className={styles.cartItemTitle}>{cartItem.Product.title}</h1>
+        </Link>
+        <div className={styles.cartItemDetails}>
+        <p >
+          Цвет: {cartItem.colorProduct.Color.title}
+        </p>
+        <p >
+          Размер: {cartItem.ProductSize.sizeTitle}
+        </p>
+        </div>
+        <div className={styles.price}>
+          <div
+            className={styles.basePrice}
+            style={{
+              textDecoration: cartItem.Product?.salePrice
+                ? "line-through"
+                : "none",
+            }}
+          >
+            {cartItem.Product?.price} ₽
+          </div>
+          {cartItem.Product?.salePrice && (
+            <div className={styles.salePrice}>
+              {cartItem.Product?.salePrice} ₽
+            </div>
+          )}
+        </div>
+        <div className={styles.cartItemCounter}>
+          <button onClick={() => onHandleDecreaseQuantity(cartItem)}>-</button>
+          <p className={styles.cartItemDetails}>{cartItem.quantity}</p>
+          <button onClick={() => onHandleIncreaseQuantity(cartItem)}>+</button>
+        </div>
       </div>
     </div>
   );
